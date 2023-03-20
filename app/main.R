1b0fb509f48181b5ade574954b7bed5f5bd2513b
FabioBedin <fabio.bedin.123@gmail.com>
2023-03-20 14:10:12 +0100

    base UI

--- app/main.R
+++ app/main.R
@@ -1,13 +1,59 @@
 box::use(
   shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput],
+  shiny.fluent[fluentPage, Stack, Dropdown.shinyInput]
+)
+
+box::use(
+  app/view/cardUI[makeCard],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    tags$h3(
-      textOutput(ns("message"))
+  fluentPage(
+    Stack(
+      style= "padding: 2rem; justify-content: space-around;",
+      horizontal = TRUE,
+      # tokens = list(childrenGap = 10),
+      tags$div(
+        style= "display: flex; align-items: center; justify-content: center;",
+        tags$span(
+          "LiverE12.5",
+          class = "ms-fontSize-32 ms-fontWeight-semibold",
+          style = "color: #323130"
+        ) ,
+        tags$span(
+          "se serve aggiungere un sottotitolo",
+          class = "ms-fontSize-14 ms-fontWeight-regular",
+          style = "color: #605E5C; margin: 14px;"
+        )
+      ),
+      tags$div(
+        style= "display: flex; align-items: center; justify-content: center;",
+        Dropdown.shinyInput(
+          style="width: 200px;",
+          inputId = ns("dropdown_input"),
+          value = "dataset1",
+          options = list(
+            list(key = "dataset1", text = "Dataset 1"),
+            list(key = "dataset2", text = "Dataset 2")
+          )
+        )
+      )
+    ),
+    Stack(
+      style= "padding: 1rem;",
+      horizontal = TRUE,
+      tokens = list(childrenGap = 15),
+      makeCard(title="card1", content="ciao ciao ciao", style="height: 550px; flex-grow: 1;"),
+      makeCard(title="card2", content="ciao", style="height: 550px; flex-grow: 1;")
+    ),
+    Stack(
+      style= "padding: 1rem;",
+      horizontal = TRUE,
+      tokens = list(childrenGap = 15),
+      makeCard(title="card3", content="ciao ciao ciao", style="height: 400px; flex-grow: 1;"),
+      makeCard(title="card4", content="ciao", style="height: 400px; flex-grow: 1;")
     )
   )
 }
---
+++ app/view/cardUI.R
@@ -0,0 +1,19 @@
+box::use(
+  shiny[NS, renderText, tags],
+  glue[glue],
+  shiny.fluent[Stack, Text]
+)
+
+
+#' @export
+makeCard <- function(title, content, size = 12, style = "") {
+  tags$div(
+    class = glue("card ms-depth-8 ms-sm12 ms-xxxl{size}"),
+    style = glue("padding: 1rem; {style}"),
+    Stack(
+      tokens = list(childrenGap = 5),
+      Text(variant = "xLarge", title, block = TRUE),
+      content
+    )
+  )
+}
